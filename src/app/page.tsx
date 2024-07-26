7047d5655eb98b626cc29471de28a13eaee62173
Nella1a <m.allen@gmx.at>
2024-07-26 18:35:24 +0200

    Update imports and add props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 import Link from 'next/link';
-import ButtonLarge from './components/ButtonLarge';
+import Button from './components/Button';
 
 export default function Home() {
   return (
-    <section className="border max-w-screen-lg m-auto h-screen flex flex-col items-center justify-center gap-12">
+    <section className="max-w-screen-lg m-auto h-screen flex flex-col items-center justify-center gap-12">
       <h1 className="text-2xl font-bold text-neon-green">Advice Generator</h1>
       <div className="flex flex-col justify-start gap-4 basis-2/5 md:flex-row md:items-start md:gap-6 ">
         <Link href="/advice/random">
-          <ButtonLarge text={'Get Random Advice'} />
+          <Button text={'Get Random Advice'} addCss={'w-44 h-12 rounded-md'} />
         </Link>
         <Link href="/advice/search">
-          <ButtonLarge text={'Search Advice'} />
+          <Button text={'Search Advice'} addCss={'w-44 h-12 rounded-md'} />
         </Link>
       </div>
     </section>
