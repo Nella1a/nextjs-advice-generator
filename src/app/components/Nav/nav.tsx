ce6d7a79fa678644a5d3401815a94c06099824ee
Nella1a <m.allen@gmx.at>
2024-07-26 18:29:37 +0200

    Update nav - fix links and update css

--- src/app/components/Nav/nav.tsx
+++ src/app/components/Nav/nav.tsx
@@ -2,16 +2,16 @@ import Link from 'next/link';
 
 const Navigation = () => {
   return (
-    <nav className="border border-neon-green h-full max-w-screen-lg m-auto flex items-center">
-      <ul className=" flex text-neon-green w-full mx-4 gap-8">
-        <li className="border border-white hover:text-light-cyan">
+    <nav className="h-full max-w-screen-lg m-auto flex items-center">
+      <ul className=" flex text-neon-green w-full mx-4 gap-4">
+        <li className="hover:text-light-cyan">
           <Link href="/">Home</Link>
         </li>
-        <li className="border border-white ml-auto hover:text-light-cyan">
+        <li className="ml-auto hover:text-light-cyan">
           <Link href="/advice/search">Search</Link>
         </li>
         <li className="hover:text-light-cyan">
-          <Link href="/advice/search">Random</Link>
+          <Link href="/advice/random">Random</Link>
         </li>
       </ul>
     </nav>
