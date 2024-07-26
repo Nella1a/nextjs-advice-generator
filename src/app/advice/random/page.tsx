9dc0284b3d3e999f85f7cf6c58e71ddd11d59527
Nella1a <m.allen@gmx.at>
2024-07-26 18:34:31 +0200

    Add client component - RandomAdvice

--- src/app/advice/random/page.tsx
+++ src/app/advice/random/page.tsx
@@ -1,4 +1,86 @@
+'use client';
+
+import Button from '@/app/components/Button';
+import { useEffect, useState } from 'react';
+import useSWR from 'swr';
+
+export interface ErrorMessage {
+  message: { type: string; text: string };
+}
+
+export interface RandomAdvice {
+  slip: { id: number; advice: string };
+}
+
+// todo: to many states, find better solution
+
 const RandomAdvice = () => {
-  return <>Hallo Random Advice</>;
+  const [shouldFetch, setShouldFetch] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [advice, setAdvice] = useState<RandomAdvice>();
+
+  const fetcher = (args: string) =>
+    fetch(args).then((res) => {
+      setShouldFetch(false);
+      return res.json();
+    });
+
+  const { data, error } = useSWR<RandomAdvice, ErrorMessage>(
+    shouldFetch ? 'https://api.adviceslip.com/advice' : null,
+    fetcher,
+  );
+
+  const onClickHandler = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      //Note: Advice is cached for 2 seconds. Any repeat-request within 2 seconds will return the same piece of advice.
+      setShouldFetch(true);
+      setIsLoading(false);
+    }, 1000);
+  };
+
+  useEffect(() => {
+    if (data) setAdvice(data);
+  }, [data]);
+
+  return (
+    <section className="max-w-screen-lg m-auto h-screen flex flex-col items-center justify-start gap-8">
+      <h1 className="text-xl font-bold text-neon-green mt-32">Random Advice</h1>
+
+      <div
+        className="flex flex-col max-h-[15rem] w-11/12
+      justify-start basis-3/4 gap-4
+      bg-dark-grayish-blue rounded-md relative font-manrope p-4  text-center drop-shadow-xl md:w-7/12"
+      >
+        <h2 className=" text-neon-green  text-center font-semibold text-base">
+          {advice && `# ${advice?.slip.id}`}
+        </h2>
+
+        <p className="w-full flex justify-center basis-2/5 items-start text-quote-size text-neon-green  m-auto font-semibold">
+          {isLoading ? (
+            <span>is loading...</span>
+          ) : !advice ? (
+            <span className="text-light-cyan">
+              There is currently no advice!
+            </span>
+          ) : !error ? (
+            <span> &#8220;{advice?.slip.advice}&#8221;</span>
+          ) : (
+            <span className={'text-red-600'}>
+              Oops, something went wrong. Please try again!
+              {error?.message.text}
+            </span>
+          )}
+        </p>
+      </div>
+
+      <div className="flex justify-center">
+        <button onClick={onClickHandler}>
+          <Button text={'Get Advice'} addCss={'w-32 h-12 rounded-md'} />
+        </button>
+      </div>
+    </section>
+  );
 };
+
 export default RandomAdvice;
