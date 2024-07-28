7bb72be2033c9ed7eb21586a25e267f78ad2d8be
Nella1a <m.allen@gmx.at>
2024-07-28 17:21:27 +0200

    Add indicator for saving advice

--- src/app/advice/search/components/SearchResult/index.tsx
+++ src/app/advice/search/components/SearchResult/index.tsx
@@ -1,10 +1,11 @@
-import { RandomAdviceResponse } from '@/app/advice/random/page';
+import Heart from '@/app/components/Icons/Heart';
 import { useState } from 'react';
-import { SearchAdviceResponse } from '../../page';
+import { AdviceToSave, SearchAdviceResponse } from '../../page';
 import Pagination from '../Pagination';
 
 const SearchResults = ({ result }: { result: SearchAdviceResponse }) => {
   const [currentPage, setCurrentPage] = useState(1);
+  const [initialResult, setInitialResult] = useState(result?.slips);
   const pageSize = 3;
 
   const onPageChange = (page: number) => {
@@ -12,7 +13,7 @@ const SearchResults = ({ result }: { result: SearchAdviceResponse }) => {
   };
 
   const paginate = (
-    items: RandomAdviceResponse[],
+    items: AdviceToSave[],
     pageNumber: number,
     pageSize: number,
   ) => {
@@ -20,20 +21,43 @@ const SearchResults = ({ result }: { result: SearchAdviceResponse }) => {
     return items.slice(startIndex, startIndex + pageSize);
   };
 
+  const saveAdviceHandler = (id: number, index: number) => {
+    const newResult = initialResult.map((advice) => {
+      if (advice.id === id) {
+        if (advice?.toSave) advice.toSave = !true;
+        else advice.toSave = true;
+      }
+      return advice;
+    });
+    setInitialResult(newResult);
+  };
+
   return (
     <>
-      <article className="border-2 flex flex-col justify-center items-center text-white mt-8 mx-4 gap-6 w-11/12 md:w-6/12">
-        {paginate(result.slips, currentPage, pageSize).map((advice: any) => {
-          return (
-            <div
-              key={`${advice.id}`}
-              className="flex flex-col items-center gap-2 text-white-500  bg-dark-grayish-blue rounded-xl p-4 drop-shadow-xl w-full min-h-28"
-            >
-              <span className={'text-neon-green font-bold'}>#{advice.id} </span>
-              {advice.advice}
-            </div>
-          );
-        })}
+      <article className="flex flex-col justify-center items-center text-white mt-4 mx-4 gap-6 w-11/12 md:w-6/12">
+        {paginate(initialResult, currentPage, pageSize).map(
+          (advice: AdviceToSave, index) => {
+            return (
+              <div
+                key={`${advice.id}`}
+                className="flex flex-col items-center gap-2 text-white-500  bg-dark-grayish-blue rounded-xl p-4 drop-shadow-xl w-full min-h-28 relative"
+              >
+                <span onClick={() => saveAdviceHandler(advice.id, index)}>
+                  <Heart
+                    className={`size-6 absolute top-2 right-3`}
+                    fill={`${
+                      advice.toSave && advice.toSave ? '#52ffa8' : 'none'
+                    }`}
+                  />
+                </span>
+                <span className={'text-neon-green font-bold'}>
+                  #{advice.id}{' '}
+                </span>
+                {advice.advice}
+              </div>
+            );
+          },
+        )}
       </article>
       <Pagination
         totalSearchResult={result.total_results}
