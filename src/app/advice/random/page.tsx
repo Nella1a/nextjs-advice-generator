dbb884e00fbbae25bd45dd72b9b6622d6cfc4af2
Nella1a <m.allen@gmx.at>
2024-08-06 21:46:18 +0200

    Clean up

--- src/app/advice/random/page.tsx
+++ src/app/advice/random/page.tsx
@@ -18,7 +18,7 @@ export interface RandomAdviceResponse {
 }
 const fetcher = (args: string) => fetch(args).then((res) => res.json());
 
-// todo: dont show previous advice
+// todo: don't show previous advice
 
 const RandomAdvice = () => {
   const [shouldFetch, setShouldFetch] = useState(false);
@@ -44,8 +44,7 @@ const RandomAdvice = () => {
       setShouldFetch(false);
     }
   }, [data]);
-  console.log('data: ', data);
-  console.log('shouldfetch: ', shouldFetch);
+
   return (
     <section className="max-w-screen-lg m-auto h-screen flex flex-col items-center justify-start gap-8">
       <h1 className="text-3xl font-bold text-light-cyan mt-16">
