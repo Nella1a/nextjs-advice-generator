053ff079caa6d93cd94260bff0bd5098684ba3f2
Nella1a <m.allen@gmx.at>
2024-08-20 22:33:33 +0200

    Save advice to DB

--- src/app/api/save/route.ts
+++ src/app/api/save/route.ts
@@ -1,8 +1,29 @@
+import { AdviceToSave } from '@/app/advice/search/page';
 import { NextRequest, NextResponse } from 'next/server';
+import prisma from '../../../../prisma';
 
 export async function POST(request: NextRequest) {
-  const body = await request.json();
+  const body: AdviceToSave = await request.json();
   console.log('body: ', body);
 
+  // todo: check types
+  const foundAdvice = await prisma.advice.findUnique({
+    where: {
+      externalId: body.id,
+    },
+  });
+
+  if (!foundAdvice) {
+    const savedAdvice = await prisma.advice.create({
+      data: {
+        externalId: body.id,
+        advice: body.advice,
+        user: 1,
+      },
+    });
+    console.log('saved Advice: ', savedAdvice);
+  }
+
+  console.log('found Advice: ', foundAdvice);
   return NextResponse.json(body);
 }
