fe84043aecffb7255ab85a8ff00b0bcd301334db
Nella1a <m.allen@gmx.at>
2024-08-20 20:18:40 +0200

    Add prisma client

---
+++ prisma.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from '@prisma/client';
+
+/*
+instance PrismaClient and save it on the globalThis object. Then we keep a check to only instantiate PrismaClient if it's not on the globalThis object otherwise use the same instance again if already present to prevent instantiating extra PrismaClient instances.
+*/
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
